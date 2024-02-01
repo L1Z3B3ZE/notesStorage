bf4a2a3ad44b58bc8c34d2323c01a87281b5b1a6
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-02-02 00:13:57 +0700

    Валидация количества пунктов заметки

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ let app = new Vue({
             const newTask = {
                 id: Date.now(),
                 title: this.taskTitle,
-                items: this.newItemText.split('\n').filter(item => item.trim() !== '').map(item => ({
-                    text: item,
-                    checked: false
-                }))
+                items: this.newItemText.split('\n').filter(item => item.trim() !== '').map(item => ({ text: item, checked: false }))
             };
+            if (this.taskTitle !== '' && newTask.items.length >= 3 && newTask.items.length <= 5) {
+                this.column1.push(newTask);
 
-            this.column1.push(newTask);
-
-        }
+            } else alert("В заметке может быть минимум 3 и максимум 5 пунктов!!")
+            this.taskTitle = '';
+            this.newItemText = '';
+        },
     },
 })
