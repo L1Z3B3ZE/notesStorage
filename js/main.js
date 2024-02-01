8a65274974f78cbaaa72921e4bfb7f27be23afff
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-02-02 00:08:19 +0700

    Реализация добавления заметки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 let app = new Vue({
     el: '#app',
     data: {
+        newItemText: '',
         taskTitle: '',
         column1: [],
         column2: [],
         column3: [],
-    }
+    },
+    methods: {
+        addTask() {
+            const newTask = {
+                id: Date.now(),
+                title: this.taskTitle,
+                items: this.newItemText.split('\n').filter(item => item.trim() !== '').map(item => ({
+                    text: item,
+                    checked: false
+                }))
+            };
+
+            this.column1.push(newTask);
+
+        }
+    },
 })
