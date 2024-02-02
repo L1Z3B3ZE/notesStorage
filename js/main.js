755fbd05d59561caab88f494e11b1de3edc027ec
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-02-02 16:18:43 +0700

    изменения кода

--- js/main.js
+++ js/main.js
@@ -10,27 +10,6 @@ let app = new Vue({
     },
     methods: {
         addTask() {
-            if (this.taskTitle !== '' && this.column1.length < 3) {
-                const newTask = {
-                    id: Date.now(),
-                    title: this.taskTitle,
-                    items: this.newItemText.split('\n').filter(item => item.trim() !== '').map(item => ({ text: item, checked: false }))
-                };
-                if (this.taskTitle !== '' && newTask.items.length >= 3 && newTask.items.length <= 5) {
-                    this.column1.push(newTask);
-
-                } else alert("В заметке может быть минимум 3 и максимум 5 пунктов!!")
-                this.taskTitle = '';
-                this.newItemText = '';
-                this.saveLocalStorage()
-            } else {
-                alert("в столбце находится максимальное число карточек")
-                this.taskTitle = '';
-                this.newItemText = '';
-
-            }
-        },
-        addTask() {
             if(this.column1.length >= 3){
                 alert('пупупу так нельзя здесь уже три карточки')
                 return
